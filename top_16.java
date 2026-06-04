953a988596b0ce78fe54cbba65c627d67a39a012
Pham Huy <mincraft0503@gmail.com>
2026-06-05 00:09:34 +0700

    push top_16 up

---
+++ top_16.java
@@ -0,0 +1,16 @@
+public class top_16 {
+    static int arrayCountGreaterThan(int a[], int b) {
+        assert(a != null);
+        int cnt = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > b) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+    public static void main(String[] unused) {
+        int[] a = {1, 2, 3, 4, 5, 6};
+        System.out.println(arrayCountGreaterThan(a, 2));
+    }
+}
